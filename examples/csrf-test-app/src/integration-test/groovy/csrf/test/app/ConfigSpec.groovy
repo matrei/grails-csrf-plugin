55730ae1b5a67c96f3d514912436b72cad0371cc
Mattias Reichel <mattias.reichel@gmail.com>
2024-12-28 21:56:34 +0100

    test: run tests with different grails environments

--- examples/csrf-test-app/src/integration-test/groovy/csrf/test/app/ConfigSpec.groovy
+++ examples/csrf-test-app/src/integration-test/groovy/csrf/test/app/ConfigSpec.groovy
@@ -3,23 +3,15 @@ package csrf.test.app
 import grails.testing.mixin.integration.Integration
 import io.github.matrei.grailsplugin.csrf.CsrfConfig
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Profile
+import spock.lang.IgnoreIf
 import spock.lang.Specification
 
 @Integration
-@Profile('configtest')
+@IgnoreIf({ System.getProperty('geb.env') != 'configtest' })
 class ConfigSpec extends Specification {
 
     @Autowired
     CsrfConfig csrfConfig
-
-    void setupSpec() {
-        System.setProperty('grails.env', 'configtest')
-    }
-
-    void cleanupSpec() {
-        System.clearProperty('grails.env')
-    }
 
     void 'the config is loaded'() {
         expect:
